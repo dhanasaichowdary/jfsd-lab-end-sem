d8a84457de422b1e15b5926901d3f93271dc559e
dhanasaichowdary <151798984+dhanasaichowdary@users.noreply.github.com>
2024-12-07 12:26:17 +0530

    Add files via upload

---
+++ customer-management/src/main/java/com/klef/jfsd/exam/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package com.klef.jfsd.exam.controller;
+
+import com.klef.jfsd.exam.model.Customer;
+import com.klef.jfsd.exam.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+}
